7aa6cf198a5067dea495d6c5f7ab7a87e4489716
Alexander Kipry <a.kipry@posteo.de>
2022-12-19 22:31:21 +0100

    1

--- Angebotsverwaltung.java
+++ Angebotsverwaltung.java
@@ -1,8 +1,11 @@
-
+// Importiert die Klasse Vector
+import java.util.Vector;
 /**
  * Bei der Klasse Angebotsverwaltung handelt es sich um eine Verwaltungsklasse, welche diverse Funktionen in Bezug auf die Klassen "Kunstinstallationen", "Bild" und "Kunstgegenstand" durchführen kann. 
  * So kann diese die o.g. Klassen nach Objekten durchsuchen und gezielt darauf zugreifen.  
- * Die Angebotsverwaltung ist zudem geeignet die aus dem Angebot hervorgehenden Kunstwerke nach Attraktivität zu sortieren und ist durch weitere Methoden in der Lage die Klasse der "Ausstellungsplanung" zu entlasten. 
+ * Die Angebotsverwaltung ist zudem geeignet die aus dem Angebot hervorgehenden Kunstwerke nach Attraktivität zu sortieren
+ * Aufgrunddessen ist sie in der Lage die Klasse der "Ausstellungsplanung" zu entlasten. 
+ * 
  * Die Ausstellungsplanung kann sich folglich auf die Angebotsverwaltung beziehen, um daraus die richtige Auswahl der Ausstellung und Ausleihe zu treffen. 
  * 
  * @author (Alexander Kipry) 
@@ -11,17 +14,51 @@
 
 public class Angebotsverwaltung
 {
-    
-    private Angebotsverwaltung angebot;
+    /** In diesem Attribut werden die einzelnen Kunstwerke aus dem Angebot mittels der der Containerklasse Vektor verwaltet. */
+    private Vector<Kunstwerk> kunstwerkVector;
 
     /**
-     * Konstruktor für Objekte der Klasse Angebotsverwaltung
+     * Konstruktor für Objekte der Klasse Angebotsverwaltung. Dieser Konstruktor erzeugt ein leeres Angebot, welches später über Methoden mit Kunstwerken befüllt werden kann. 
      */
     public Angebotsverwaltung()
     {
-        // Instanzvariable initialisieren
+       //anlegen eines neuen Vectorobjektes
+       kunstwerkVector = new Vector<Kunstwerk>();
     }
-
+    //Methoden für die Verwaltung der Klasse Angebotsverwaltung
+    /**
+     * Diese Methode fügt der Angebotsverwaltung ein Kunstwerk hinzu.
+     */
+    public void addKunstwerk()
+    {
+        //Code zum Hinzufügen eines Kunstwerkes (Kunstinstallation, Bild, Kunstgegenstand)
+    }
+    /** 
+     * Diese Methode entfernt aus der Angebotsverwaltung ein Kunstwerk. 
+     */
+    public void removeKunstwerk()
+    {
+        //Code zum Entfernen eines Kunstwerkes (Kunstinstallation, Bild, Kunstgegenstand)
+    }
+    /** Ermittelt die Anzahl an Kunstwerken in der Angebotsverwaltung .
+     *  
+     *  @return Die Anzahl der Objekte der Kunstwerk-Klassen in der Angebotsverwaltung. 
+     */
+    public int sizeAngebotsverwaltung()
+    {
+        return kunstwerkVector.size();
+    }
+    /** Loescht alle Kunstwerke aus der Angebotsverwaltung.
+     * 
+     */
+    public void clearAngebotsverwaltung()
+    {
+        //Code zum Löschen aller Kunstwerke aus der Angebotsverwaltung
+    }
+    
+    
+    
+    //Methoden mit allgemeinen Suchfunktionen
     /**
      * Diese Methode durchsucht die Objekte nach einer bestimmten Kunstinstallation und zeigt dieses an.
      */
