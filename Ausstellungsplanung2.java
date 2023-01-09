5ac0bdeefe30ccaaec9404e53533353bc0970ac7
Mischa Marchlewski <mischa-paul.marchlewski@stud.uni-bamberg.de>
2023-01-09 15:41:35 +0100

    -

--- Ausstellungsplanung2.java
+++ Ausstellungsplanung2.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Vector;
+import java.util.*;
 
 /**
  * Beschreiben Sie hier die Klasse Ausstellungsplanung2.
@@ -10,7 +9,7 @@ import java.util.Vector;
 public class Ausstellungsplanung2
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private HashMap<Raum, Kunstwerk> zugeordneteKunstwerke;
+    private HashMap<Raum, List<Kunstwerk>> zugeordneteKunstwerke;
     private Raumverwaltung raeume;
     private Angebotsverwaltung kunstwerke;
 
@@ -33,9 +32,41 @@ public class Ausstellungsplanung2
     public void generiereAusstellung()
     {
         Vector<Raum> raeume2 = raeume.getRaumVector();
+        Vector<Kunstwerk> kunstwerke2 = kunstwerke.sortAttraktivitaet();
+        
+        // Abstand der Kunstwerke von den Ecken eines Raums muss 1 Meter (100 cm) entsprechen
+        int abstandEcke = 100;
         
         for(Raum raum : raeume2) 
         {
+            // Ermitelt die Länge und Breite des aktuellen Raums
+            int laengeNord = raum.getLaengeRaum();
+            int laengeSued = raum.getLaengeRaum();
+            int breiteOst  = raum.getBreiteRaum();
+            int breiteWest = raum.getBreiteRaum();
+            
+            // Türbreiten der vier Seiten ermitteln;
+            int tuerbreiteNord = raum.getTuerNord();
+            int tuerbreiteSued = raum.getTuerSued();
+            int tuerbreiteOst  = raum.getTuerOst();
+            int tuerbreiteWest = raum.getTuerWest();
+            
+            // Höhe des Raums ermitteln
+            int hoeheRaum = raum.getHoeheRaum();
+            
+            // verfügbare Nettolänge einer Wand berechnen: Nettolänge = Bruttolänge - (abstandEcke * 2) - Türbreite
+            int nettoLaengeNord = laengeNord - (abstandEcke * 2) - tuerbreiteNord;
+            int nettoLaengeSued = laengeSued - (abstandEcke * 2) - tuerbreiteSued;
+            int nettoBreiteOst  = breiteOst  - (abstandEcke * 2) - tuerbreiteOst;
+            int nettoBreiteWest = breiteWest - (abstandEcke * 2) - tuerbreiteWest;
+            
+            for(Kunstwerk kunstwerk : kunstwerke2)
+            {
+                
+            }
+            
+            
+            
             System.out.println(raum);
         }
     }
