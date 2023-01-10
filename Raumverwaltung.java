ba4e3ed7d6a4b4abde9c063f426df40f16da55e0
Thomas <thomas.scheidt@rwth-aachen.de>
2023-01-10 17:18:34 +0100

    f

--- Raumverwaltung.java
+++ Raumverwaltung.java
@@ -151,34 +151,6 @@ public class Raumverwaltung
     
     
     
-    // Soll raus aus dieser Klasse:
-    /** Ermittelt alle Objekte der Klasse Raum, die mindestens ein Kunstwerk entsprechend dem Schwerpunktthema enthalten.
-     *  @return     Objekte der Klasse Raum, das Kriterien in Schwerpunktthema erfüllt.
-     */
-        private Vector<Raum> getRaumThema1()
-    {
-        //Code einfuegen
-        return raumVector;
-    }
     
-    /** Ermittelt alle Objekte der Klasse Raum, die kein Kunstwerk entsprechend dem Schwerpunktthema enthalten.
-     *  @return     Objekte der Klasse Raum, das Kriterien in Schwerpunktthema nicht erfüllt.
-     */
-        private Vector<Raum> getRaumThema2()
-    {
-        //Code einfuegen
-        return raumVector;
-    }
-    
-    /** Ueberprueft, ob mindestens die Hälfte aller Raeume mindestens ein Kunstwerk mit dem Schwerpunktthema enthalten.
-     *  @return     Wahrheitswert, ob Bedingung erfuellt ist.
-     */
-        public boolean pruefeVertretungThema()
-    {
-        //Code einfuegen
-        //"true" falls size raumVector der Methode getRaumThema1 >= size raumVector der Methode getRaumThema2
-        //"false" falls size raumVector der Methode getRaumThema1 < size raumVector der Methode getRaumThema2
-        return true;
-    }
     
 }
--- Zuordnung.java
+++ Zuordnung.java
@@ -343,6 +343,37 @@ public class Zuordnung
         //"false" falls verschiedene Themen pro Raum > 3
         return true;
     }
+    
+    
+    /** Ermittelt alle Objekte der Klasse Raum, die mindestens ein Kunstwerk entsprechend dem Schwerpunktthema enthalten.
+     *  @return     Objekte der Klasse Raum, das Kriterien in Schwerpunktthema erfüllt.
+     */
+        private Vector<Raum> getRaumThema1()
+    {
+        //Code einfuegen
+        return raumVector;
+    }
+    
+    /** Ermittelt alle Objekte der Klasse Raum, die kein Kunstwerk entsprechend dem Schwerpunktthema enthalten.
+     *  @return     Objekte der Klasse Raum, das Kriterien in Schwerpunktthema nicht erfüllt.
+     */
+        private Vector<Raum> getRaumThema2()
+    {
+        //Code einfuegen
+        return raumVector;
+    }
+    
+    /** Ueberprueft, ob mindestens die Hälfte aller Raeume mindestens ein Kunstwerk mit dem Schwerpunktthema enthalten.
+     *  @return     Wahrheitswert, ob Bedingung erfuellt ist.
+     */
+        public boolean pruefeVertretungThema()
+    {
+        //Code einfuegen
+        //"true" falls size raumVector der Methode getRaumThema1 >= size raumVector der Methode getRaumThema2
+        //"false" falls size raumVector der Methode getRaumThema1 < size raumVector der Methode getRaumThema2
+        return true;
+    }
+    
         /**
      * Map,die Räume und ihre im Rahmen der Planung zugeordneten Kunstwerke enthält. Der Raum wird 
      * als Schlüssel genommen (da eindeutig und nicht mehrmals vorhanden) und die
