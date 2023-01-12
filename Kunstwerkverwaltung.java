82a9e3fab97d552957e94a62b55c823568cbcc3f
Alexander Kipry <kipry@murena.io>
2023-01-12 21:52:00 +0100

    1

--- Kunstwerkverwaltung.java
+++ Kunstwerkverwaltung.java
@@ -1,17 +1,18 @@
 // Importiert die Klasse Vector
 import java.util.Vector;
 import java.util.*;
+import java.util.Comparator;
 
 /**
- * Bei der Klasse Angebotsverwaltung handelt es sich um eine Verwaltungsklasse, welche diverse Funktionen in Bezug auf die Klassen "Kunstinstallationen", "Bild" und "Kunstgegenstand" durchführen kann. 
+ * Bei der Klasse Kunstwerkverwaltung handelt es sich um eine Verwaltungsklasse, welche diverse Funktionen in Bezug auf die Klassen "Kunstinstallationen", "Bild" und "Kunstgegenstand" durchführen kann. 
  * So kann diese die o.g. Klassen nach Objekten durchsuchen und gezielt darauf zugreifen.  
- * Die Angebotsverwaltung ist zudem geeignet die aus dem Angebot hervorgehenden Kunstwerke nach Attraktivität zu sortieren
+ * Die Kunstwerkverwaltung ist zudem geeignet die Logikklassen gezielt durch Methoden zu unterstützen. 
  * Aufgrunddessen ist sie in der Lage die Klasse der "Ausstellungsplanung" zu entlasten. 
  * 
- * Die Ausstellungsplanung kann sich folglich auf die Angebotsverwaltung beziehen, um daraus die richtige Auswahl der Ausstellung und Ausleihe zu treffen. 
+ * Die Ausstellungsplanung kann sich folglich auf die Kunstwerkverwaltung beziehen, um daraus die richtige Auswahl der Ausstellung und Ausleihe zu treffen. 
  * 
  * @author (Alexander Kipry) 
- * @version (18.12.2022)
+ * @version (12.01.2023)
 */
 
 public class Kunstwerkverwaltung
@@ -20,16 +21,16 @@ public class Kunstwerkverwaltung
     private Vector<Kunstwerk> kunstwerkVector;
    
     /**
-     * Konstruktor für Objekte der Klasse Angebotsverwaltung. Dieser Konstruktor erzeugt ein leeres Angebot, welches später über Methoden mit Kunstwerken befüllt werden kann. 
+     * Konstruktor für Objekte der Klasse Kunstwerkverwaltung. Dieser Konstruktor erzeugt ein leeres Angebot, welches später über Methoden mit Kunstwerken befüllt werden kann. 
      */
     public Kunstwerkverwaltung()
     {
        //anlegen eines neuen Vectorobjektes
        kunstwerkVector = new Vector<Kunstwerk>();
     }
-    //Methoden für die Verwaltung der Klasse Angebotsverwaltung
+    //Methoden für die Vektorliste der Klasse Kunstwerkverwaltung 
     /**
-     * Diese Methode fügt der Angebotsverwaltung ein Kunstwerk hinzu.
+     * Diese Methode fügt dem Vector der Kunstwerkverwaltung ein Kunstwerk hinzu.
      * 
      * @param kunstwerk  Kunstwerk (Kunsinstallation, Bild, Kunstgegenstand) der hinzugefügt werden soll
      */
@@ -39,32 +40,31 @@ public class Kunstwerkverwaltung
     }
     
     /** 
-     * Diese Methode entfernt aus der Angebotsverwaltung ein Kunstwerk. 
+     * Diese Methode entfernt aus dem Vektor der Kunstwerkverwaltung ein Kunstwerk. 
      * 
      * @param kunstwerk Kunstwerk das gelöscht werden soll
      */
     public void removeKunstwerk(Kunstwerk in_kunstwerk)
     {
-        //Code zum Entfernen eines Kunstwerkes (Kunstinstallation, Bild, Kunstgegenstand)
+        kunstwerkVector.remove(in_kunstwerk);
     }
     
-    /** Ermittelt die Anzahl an Kunstwerken in der Angebotsverwaltung .
+    /** Ermittelt die Anzahl an Kunstwerken in der Vektorliste der Kunstwerkverwaltung .
      *  
-     *  @return Die Anzahl der Objekte der Kunstwerk-Klassen in der Angebotsverwaltung. 
+     *  @return Die Anzahl der Objekte der Kunstwerk-Klassen in der Kunstwerkverwaltung. 
      */
-    public int sizeAngebotsverwaltung()
+    public int sizeKunstwerkverwaltung()
     {
         return kunstwerkVector.size();
     }
     
-    /** Loescht alle Kunstwerke aus der Angebotsverwaltung.
+    /** Loescht alle Kunstwerke aus dem Vektor der Kunstwerkverwaltung.
      * 
      */
-    public void clearAngebotsverwaltung()
+    public void clearKunstwerkverwaltung()
     {
-        //Code zum Löschen aller Kunstwerke aus der Angebotsverwaltung
+        kunstwerkVector.clear();
     }
-    
     
     /**
      * Methode zur Umsetzung des Aufrufens aller Kunstwerke innerhalb der Vektorliste 
@@ -78,22 +78,24 @@ public class Kunstwerkverwaltung
             System.out.println(kw);
         }
     }
-
-
+    
     /**
      * Methode zum Sortieren der Kunstwerke aus dem Vector nach deren Kosten. 
      * 
-     * @return sortiert die Kunstwerke innerhalb des Vectors nach deren Kosten. 
+     * @return Gibt eine nach Kosten sortierte Liste der Kunstwerke innerhalb des Vectors zurück. 
      */
-    public Vector<Kunstwerk> sortKosten()
+    public void sortKosten()
     {
-        return kunstwerkVector;//Code zum sortieren der Kunstwerke nach den Kosten --> Collections.sort mit Parameter kunstwerk.kosten 
+        Collections.sort(kunstwerkVector);
+        for (Kunstwerk kw: kunstwerkVector){
+            System.out.println(kw);//Code zum sortieren der Kunstwerke nach den Kosten --> Collections.sort mit Parameter kunstwerk.kosten 
+        }
     }
     
     /**
      * Methode zum Sortieren der Kunstwerke aus dem Vector nach deren Attrakvitaet. 
      * 
-     * @return sortiert die Kunstwerke innerhalb des Vectors nach deren Attrakvitaet. 
+     * @return Gibt eine nach Attraktivitaet sortierte Liste der Kunstwerke innerhalb des Vectors zurück.
      */
     public Vector<Kunstwerk> sortAttraktivitaet()
     {
