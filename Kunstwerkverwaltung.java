2349fbc56c8b9a12ede096a605d7396365b824fd
Alexander Kipry <kipry@murena.io>
2023-01-24 16:40:15 +0100

    keine Plausifehler mehr

--- Kunstwerkverwaltung.java
+++ Kunstwerkverwaltung.java
@@ -135,7 +135,7 @@ public class Kunstwerkverwaltung
      */
     public boolean checkPlatzfürKG (int in_hoehe, int in_breite)
     {
-        if {return false; //muss noch geschrieben werden
+         return false; //muss noch geschrieben werden
     }
     /**
      * Methode, welche überprüft, ob noch genügend Platz für das Bild in einem Raum übrig ist
