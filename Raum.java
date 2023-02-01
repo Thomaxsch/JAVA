a21b5bfefc959f5ccf3f05311e92e8b1a44f32d3
Carla Saradeth <charly_saradeth@yahoo.de>
2023-02-01 09:04:08 +0100

    h

--- Raum.java
+++ Raum.java
@@ -343,8 +343,7 @@ public class Raum
     public String toString()
     {
         return "Nummer: " + this.nummer + ", Raumbezeichnung: " + this.name + ", Breite: " + this.breiteRaum +
-        ", Länge: " + this.laengeRaum + ", Höhe:" + this.hoeheRaum;  
-
+        ", Länge: " + this.laengeRaum + ", Höhe: " + this.getHoeheRaum();  
     }
     
     /** Gibt eine lesbare Beschreibung eines Raumes aus (fuer Museumsfuehrer und Ausleihdatei)
